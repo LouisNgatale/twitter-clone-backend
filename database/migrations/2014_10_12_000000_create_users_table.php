3edd9bf2354f81ec0d821ff879d4fd5ac546f259
Louis Ngatale <louisngatale@gmail.com>
2021-08-24 13:51:17 +0300

    Laravel sanctum setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,12 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
+            $table->date('dob');
+            $table->unsignedBigInteger('phone_number');
+            $table->string('profile_image');
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
