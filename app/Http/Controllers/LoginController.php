7c4b109445adfd89fee9fc4a9768f7f126363193
Louis Ngatale <louisngatale@gmail.com>
2021-08-25 21:49:33 +0300

    Login and generate authentication token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -110,6 +110,8 @@ class LoginController extends Controller
             }else{
                 return response()->json(['error' => "Username not found"], 201);
             }
+
+            // Assign token
         }
         else{
             return response()->json(['error' => "Invalid request"], 201);
